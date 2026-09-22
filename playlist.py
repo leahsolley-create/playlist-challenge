479e0db241268114ae98fd93d28ef054fea303da
leahsolley-create <leah.solley@ascte.org>
2026-09-22 08:16:40 -0500

    added_new_song

--- playlist.py
+++ playlist.py
@@ -4,6 +4,7 @@ SONGS = [
     ("Imagine", "John Lennon"),
     ("Blinding Lights", "The Weeknd"),
     ("Viva La Vida", "Coldplay"),
+    ("Bohemian Rhapsody", "Queen"),
 ]
 
 def print_playlist(songs):
